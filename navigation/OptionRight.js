e7cdb0e064b8d28bba5fab556c482d64dd064cf2
Kennette Canales <kennettecanales@gmail.com>
2020-07-10 15:35:34 +0800

    update

--- navigation/OptionRight.js
+++ navigation/OptionRight.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, Platform, Dimensions } from 'react-native';
 import {NavigationActions} from 'react-navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faEnvelope, faBell, faBus, faMapMarker, faUsers} from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faMapMarker } from '@fortawesome/free-solid-svg-icons';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { Color, BasicStyles } from 'common';
 
@@ -31,71 +31,34 @@ class NavigationDrawerStructureRight extends Component {
   render() {
     const { notifications, user, activeRoute } = this.props.state;
     return (
-      <View style={{ flexDirection: 'row', width: width - (width * .20)}}>
+      <View style={{
+        flexDirection: 'row',
+        width: width - (width * .10)
+      }}>
+
 
         <View style={{
-          width: '25%'
+          width: '85%',
+          flexDirection: 'row'
+        }}>
+          <FontAwesomeIcon icon={ faMapMarker } size={15} style={[BasicStyles.iconStyle, {
+                color: Color.primary
+              }]}/>
+          <Text style={{
+            fontSize: 12
+          }}>
+            Casili, Casili, Consolacion
+          </Text>
+        </View>
+
+        <View style={{
+          width: '15%'
         }}>
           <TouchableOpacity onPress={() => this.navigateToScreen('LinkedAccounts')}>
             <View style={{ flexDirection: 'row'}}>
-              <FontAwesomeIcon icon={ faUsers } size={BasicStyles.iconSize} style={[BasicStyles.iconStyle, {
-                color: activeRoute == 'LinkedAccounts' ? Color.secondary : Color.white
+              <FontAwesomeIcon icon={ faShoppingCart } size={15} style={[BasicStyles.iconStyle, {
+                color: Color.black
               }]}/>
-            </View>
-          </TouchableOpacity>   
-        </View>
-        <View style={{
-          width: '25%'
-          }}>
-          <TouchableOpacity onPress={() => this.navigateToScreen('Location')}>
-            <View style={{ flexDirection: 'row'}}>
-              <FontAwesomeIcon icon={ faMapMarker } size={BasicStyles.iconSize} style={[BasicStyles.iconStyle, {
-            color: activeRoute == 'Location' ? Color.secondary : Color.white
-          }]}/>
-            </View>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-          width: '25%'
-        }}>
-          <TouchableOpacity onPress={() => this.navigateToScreen('Transportation')}>
-            <View style={{ flexDirection: 'row'}}>
-              <FontAwesomeIcon icon={ faBus } size={BasicStyles.iconSize} style={[BasicStyles.iconStyle, {
-                color: activeRoute == 'Transportation' ? Color.secondary : Color.white
-              }]}/>
-            </View>
-          </TouchableOpacity>   
-        </View>
-        <View style={{
-          width: '25%'
-        }}>
-          <TouchableOpacity onPress={() => this.navigateToScreen('Notification')}>
-            <View style={{ flexDirection: 'row'}}>
-              <FontAwesomeIcon icon={ faBell } size={BasicStyles.iconSize} style={[BasicStyles.iconStyle, {
-                color: activeRoute == 'Notification' ? Color.secondary : Color.white
-              }]}/>
-              {
-                notifications && notifications.unread > 0 &&
-                (
-                  <View style={{
-                    backgroundColor: Color.danger,
-                    borderRadius: 5,
-                    fontSize: 11,
-                    marginLeft: -20
-                  }}>
-                    <Text
-                      style={{
-                        color: Color.white,
-                        paddingLeft: 5,
-                        paddingRight: 5,
-                        paddingTop: 2,
-                        paddingBottom: 2,
-                        lineHeight: 20,
-                        textAlign: 'center'
-                      }}>{notifications.unread}</Text>
-                  </View>
-                )
-              }
             </View>
           </TouchableOpacity>   
         </View>
